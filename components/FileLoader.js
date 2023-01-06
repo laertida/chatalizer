159c8cde049151efe400e99a119fc81a179fedb4
laertida <laertida@pm.me>
2023-01-06 16:28:39 -0600

    Finally parsing author text and date

--- components/FileLoader.js
+++ components/FileLoader.js
@@ -13,19 +13,20 @@ const FileLoader = () => {
     const regexDateTim = new RegExp(/\d{2}\/\d{2}\/\d{2}\s+(\d{1}|\d{2}):\d{2}\s+(p.|a.)\sm./);
 
     const keys = fileContent.match(regexKey);
-
     const messages = [];
 
     for (let id = 0; id < keys.length; id++) {
-      const key = keys[id];
 
-      let author = key.slice(key.indexOf('-'), key.indexOf(':'));
-      console.log(`author: ${author}`);
+      const key = keys[id];
+      // TODO: a problem could be that author name contains character `:`
+      // It would be better if a method is implemetated to define author
+      // before data is rendered
+      let author = key.slice(key.indexOf('-') + 2, key.indexOf(':', 15));
+
       const dateTimeArray = regexDateTim.exec(key);
       const dateTimeString = dateTimeArray[0];
 
-      const text = key.slice(key.indexOf(''));
-
+      const text = key.slice(key.indexOf(author) + author.length);
 
       const [dateValues, timeValues, indicator] = dateTimeString.split(' ');
       const [day, month, year] = dateValues.split('/');
@@ -40,6 +41,7 @@ const FileLoader = () => {
       messages.push(message);
     };
     setMessages(messages);
+    AppContext.data = messages;
   };
   const readFile = (e) => {
     const files = e.target.files;
@@ -48,9 +50,6 @@ const FileLoader = () => {
     // read only one file
     fileReader.readAsText(files[0], 'UTF-8');
 
-    fileReader.onloadstart = (e) => {
-      //console.log(e);
-    };
 
     fileReader.onload = (e) => {
       let content = e.target.result;
@@ -66,10 +65,11 @@ const FileLoader = () => {
       <input type="file" onChange={readFile} />
       {messages.map((message) => {
         return (
-          <>
-            <span>author: {message.author}</span>
-            <p key={message.id}>{message.text}</p>
-          </>
+          <p key={`div-${message.id}`}>
+            <span key={`date-${message.id}`}>date: {message.date.toString()} </span>
+            <span key={`author-${message.id}`}>author: {message.author} </span>
+            <span key={`text-${message.id}`}>text: {message.text} </span>
+          </p>
         );
       })
       }
